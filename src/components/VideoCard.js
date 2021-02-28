a0d72dec27ac6c58930b172d9acf449125dab6c1
Alex Bruno Dantas Alexandrino <alexbrunoalexandrino@gmail.com>
2021-02-27 23:58:26 -0400

    Home Page com videos

---
+++ src/components/VideoCard.js
@@ -0,0 +1,55 @@
+import { Box, Typography, Avatar, makeStyles } from '@material-ui/core'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+
+dayjs.extend(relativeTime)
+
+const useStyle = makeStyles(() => ({
+  img: {
+    width: '100%'
+  },
+  caption: {
+    fontWeight: 500,
+    /* Quando texto for maior que 2 linhas vai colocar ... */
+    display: '-webkit-box',
+    '-webkit-line-clamp': 2,
+    '-webkit-box-orient': 'vertical',
+    overflow: 'hidden'
+  }
+}))
+
+function VideoCard({item}) {
+  const classes = useStyle()
+
+  return(
+    <Box>
+      <img alt={item.title} src={item.thumb} className={classes.img}/>
+      <Box display="flex" mt="1">
+        <Avatar alt={item.authorName} src={item.authorName} >
+          SS
+        </Avatar>
+      </Box>
+        <Typography
+          className={classes.caption}
+          gutterBottom
+          variant="body1"
+          color="textPrimary"
+        >
+          {item.title}
+        </Typography>
+        
+        <Typography diplay="block" variant="body2" color="textSecondary" >
+          {item.authorName}
+        </Typography>
+
+        <Typography>
+          {`${item.views} - ${dayjs(item.updateAt).fromNow()}`}
+        </Typography>
+      <Box>
+
+      </Box>
+    </Box>
+  )
+}
+
+export default VideoCard
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,80 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
+import { Grid, Box } from '@material-ui/core';
 
-import Layout from '../components/Layout'
+import Layout from 'src/components/Layout';
+import VideoCard from 'src/components/VideoCard';
 
-export default function Home() {
+
+function Home( {data} ) {
   return (
+    /** em Box p = padding */
     <Layout title="YouTube">
-      Clone Youtube com Next.Js + Material-UI
-      <Button variant="outlined" color="secondary">Hello World</Button>
+      <Box p={2}>
+        <Grid container spacing={4}>
+          {data.map((item) => (
+            <Grid key={item.id} item xl={3} lg={3} md={4} sm={6} xs={12} >
+              <VideoCard item={item} />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </Layout>
   );
 }
+
+export async function getStaticProps() {
+  const data=[
+    { 
+      id: 1,
+      title: 'NEXT.JS  O FRAMEWORK QUE VOCE DEVERIA CONHECER[PART #01]',
+      authorId: 1,
+      authorName: 'Alex Bruno',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next01.png',
+      VideoUrl: 'url',
+      updateAt: new Date()
+    },
+    { 
+      id: 2,
+      title: 'NEXT.JS  ENTENDENDO A RENDERIZAÇÃO ',
+      authorId: 1,
+      authorName: 'Alex Bruno',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next02.png',
+      VideoUrl: 'url',
+      updateAt: new Date()
+    },
+    { 
+      id: 3,
+      title: 'NEXT.JS  ROTAS ESTATICAS E DINAMICAS',
+      authorId: 1,
+      authorName: 'Alex Bruno',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next03.png',
+      VideoUrl: 'url',
+      updateAt: new Date()
+    },
+    { 
+      id: 4,
+      title: 'NEXT.JS  ROTAS ESTATICAS E DINAMICAS',
+      authorId: 1,
+      authorName: 'Alex Bruno',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next03.png',
+      VideoUrl: 'url',
+      updateAt: new Date()
+    },
+  ]
+
+  return {
+    props: {
+      data: JSON.parse(JSON.stringify(data))
+    },
+  }
+}
+
+export default Home;
